becc1079436540c0a216013a76337151bfaeba2e
Sylvia <sylviaprabudy@gmail.com>
2020-06-07 13:12:56 -0500

    making progress. All logic works, but still not able to display it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 // Assignment Code
-//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 // function writePassword() {
 
 //var password = generatePassword();
-//var passwordText = document.querySelector("#password");
+var passwordText = document.querySelector("#password");
 
-//passwordText.value = password;
+passwordText.value = password;
 
 //}
 
-// Create funstions for each criterias
+// Generate Password CTA
+var generateBtn = document.querySelector("#generate");
+
+// Create functions for each criterias
+
 function getRandomSpecial() {
-    var special = "!@#$%^&*(){}[]<>-=_+/?"
+    var special = "!@#$%^&*(){}[]<>=-_+/?~"
     return special[Math.floor(Math.random() * special.length)];
 }
 
@@ -30,18 +33,17 @@ function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
+var passCriterias;
 
-var choices;
-
-// Generate password function starts here
-function generatePassword() {
+// Write password to the #password input
+function writePassword() {
     // Ask user how many characters of password they want to generate
     var passwordLength = window.prompt("How many characters would you like your password to contain? It has to be between 8 - 128 characters.")
 
     // Check if the answer meets the requirement
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
         window.alert("Please enter a valid answer. It has to be between 8 to 128.");
-        generatePassword();
+        writePassword();
     } else {
         // Ask if user want to include any of these criterias
         var specialChar = window.confirm("Click OK to confirm including special characters.")
@@ -52,29 +54,77 @@ function generatePassword() {
 
     // If user didn't choose any criterias 
     if (!specialChar && !numericChar && !lowerCaseChar && !upperCaseChar) {
-       choices = window.alert("You must choose at least one criteria.");
+       passCriterias = alert("You must choose at least one criteria.");
     } 
     
     // User chose all 4 criterias
     else if (specialChar && numericChar && lowerCaseChar && upperCaseChar) {
-        choices = getRandomSpecial.concat(getRandomNumber, getRandomLower, getRandomUpper);
+        passCriterias = getRandomSpecial() + getRandomNumber() + getRandomLower() + getRandomUpper();
+    }
+
+    // User chose 3 criterias 
+    else if (specialChar && numericChar && lowerCaseChar) {
+        passCriterias = getRandomSpecial() + getRandomNumber() + getRandomLower();
+    }
+    else if (specialChar && numericChar && upperCaseChar) {
+        passCriterias = getRandomSpecial() + getRandomNumber() + getRandomUpper();
+    }
+    else if (specialChar && lowerCaseChar && upperCaseChar) {
+        passCriterias = getRandomSpecial() + getRandomLower() + getRandomUpper();
+    }
+    else if (numericChar && lowerCaseChar && upperCaseChar) {
+        passCriterias = getRandomNumber() + getRandomLower() + getRandomUpper();
+    }
+
+    // User chose 2 criterias
+    else if (specialChar && numericChar) {
+        passCriterias = getRandomSpecial() + getRandomNumber();
+    }
+    else if (specialChar && lowerCaseChar) {
+        passCriterias = getRandomSpecial() + getRandomLower();
+    }
+    else if (specialChar && upperCaseChar) {
+        passCriterias = getRandomSpecial() + getRandomUpper();
+    }
+    else if (numericChar && lowerCaseChar) {
+        passCriterias = getRandomNumber() + getRandomLower();
+    }
+    else if (numericChar && upperCaseChar) {
+        passCriterias = getRandomNumber() + getRandomUpper();
+    }
+    else if (lowerCaseChar && upperCaseChar) {
+        passCriterias = getRandomLower() + getRandomUpper();
+    }
+
+    // User chose 1 criteria
+    else if (specialChar) {
+        passCriterias = getRandomSpecial();
+    }
+    else if (numericChar) {
+        passCriterias = getRandomNumber();
+    }
+    else if (lowerCaseChar) {
+        passCriterias = getRandomLower();
+    }
+    else if (upperCaseChar) {
+        passCriterias = getRandomUpper();
     };
 
-
-    // password variable is an array placeholder for user generated amount of length
-    var password = [];
+    // Password variable is an array placeholder for user generated amount of length
+    var passwordArr = [];
 
     // Random selection 
     for (var i = 0; i < passwordLength; i++) {
-        var passwordChoices = choices[Math.floor(Math.random() * choices.length)];
-        password.push(passwordChoices);
+        var passwordChoices = passCriterias[Math.floor(Math.random() * passCriterias.length)];
+        passwordArr.push(passwordChoices);
+        console.log(passCriterias);
     }
    
 
 };
 
 
-generatePassword();
+writePassword();
 
 
 
