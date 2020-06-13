a40603fb22773ad04b86d9953768f5e15582606d
Sylvia <sylviaprabudy@gmail.com>
2020-06-12 20:48:40 -0500

    rewrite function, but still has a bug when user enter anything than 8-128

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,36 +4,24 @@ var generateBtn = document.querySelector("#generate");
 // Write password in textarea
 var passwordText = document.querySelector("#password");
 
-// Create functions for each criterias
-function getRandomSpecial() {
-    var special = "!@#$%^&*(){}[]<>=-_+/?~|";
-    return special[Math.floor(Math.random() * special.length)];
-}
-
-// Get fromCharCode from Character Set http://net-comber.com/charset.html
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-// Write password function
-function writePassword() {
+// Write createPass function
+function createPass() {
+    // Create string for each criteria
+    var lowercase = "abcdefghijklmnopqrstuvwxyz"
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYW"
+    var special = "!@#$%^&*(){}[]<>=-_+/?~|"
+    var number = "0123456789"
+    var characterSet = "";
+    var password = "";
     // Ask user how many characters of password they want to generate
     var passwordLength = prompt(
         "How many characters would you like your password to contain? Enter a value between 8 to 128"
     );
-
-    // Check if the answer meets the requirement
+    //Check if the answer meets the requirement
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
         alert("Please enter a valid answer. It has to be between 8 to 128");
-        writePassword();
+        createPass();
+
     } else {
         // Ask if user want to include any of these criterias
         var specialChar = confirm(
@@ -50,48 +38,30 @@ function writePassword() {
         );
     }
 
-    var criteria = {
-        passwordLength: passwordLength,
-        specialChar: specialChar,
-        numericChar: numericChar,
-        lowerCaseChar: lowerCaseChar,
-        upperCaseChar: upperCaseChar
-    };
-
-    createPass(criteria);
-}
-
-// Generate createPass function. While length of passwordLenght isn't met yet, keep looping
-function createPass(criteria) {
-    var passwordArray = [];
-    while (passwordArray.length < criteria.passwordLength) {
-        if (criteria.specialChar) {
-            passwordArray.push(getRandomSpecial());
-        }
-        if (criteria.numericChar) {
-            passwordArray.push(getRandomNumber());
-        }
-        if (criteria.lowerCaseChar) {
-            passwordArray.push(getRandomLower());
-        }
-        if (criteria.upperCaseChar) {
-            passwordArray.push(getRandomUpper());
-        }
-    } 
-   
-    var passwordStr = passwordArray.join('');
-    generatePassword(passwordStr);
-}
-
-// Create random selections
-function randomPass(characterSet) {
-    return Math.floor(Math.random() * characterSet.length);
+    if (lowerCaseChar) {
+        characterSet += lowercase;
+    }
+    if (upperCaseChar) {
+        characterSet += uppercase;
+    }
+    if (specialChar) {
+        characterSet += special;
+    }
+    if (numericChar) {
+        characterSet += number;
+    }
+    // Generate password function. While length of passwordLenght isn't met yet, keep looping
+    for (i = 0; i < passwordLength; i++) {
+        // Create random selections
+        password += characterSet[Math.floor(Math.random() * characterSet.length)]
+    }
+    return(password);
 }
 
 // Write password to the #password input
-function generatePassword(password) {
+function writePassword(password) {
     passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () { writePassword(createPass()) });
